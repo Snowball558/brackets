216d32e3655f7f96ee4d838d696a734aab66e815
Snowball558 <doved558@gmail.com>
2022-07-20 14:10:53 +0300

    task done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 != 0) {
+    return false;
+  }
+  let array = [];
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    array.push(bracketsConfig[i].join(''))
+  }
+  let aTry = 1;
+  while (aTry) {
+    for (let j = 0; j < array.length; j++) {
+      if (str.includes(array[j])) {
+        str = str.replace(array[j], '');
+        aTry++;
+      }
+    }
+    aTry--;
+  }
+  return str.length == 0;
 }
